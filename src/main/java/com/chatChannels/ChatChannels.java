50636fda5e792ab636fe62b3a60b8bd56e862207
Wilkcraft <vallejogarijomiguel@gmail.com>
2026-04-19 19:14:20 +0200

    Add VaultHook setup in onEnable method
    Integrate VaultHook setup during plugin initialization.

--- src/main/java/com/chatChannels/ChatChannels.java
+++ src/main/java/com/chatChannels/ChatChannels.java
@@ -3,6 +3,8 @@ package com.chatChannels;
 import com.chatChannels.chat.ChatListener;
 import com.chatChannels.commands.ChannelCommand;
 import com.chatChannels.commands.ChannelTabCompleter;
+import com.chatChannels.utils.VaultHook;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChatChannels extends JavaPlugin {
@@ -23,6 +25,7 @@ public class ChatChannels extends JavaPlugin {
       getLogger().info("Discord bot not enabled (no token provided).");
     }
 
+    VaultHook.setup();
     getServer().getPluginManager().registerEvents(new ChatListener(), this);
 
     getCommand("channel").setExecutor(new ChannelCommand());
